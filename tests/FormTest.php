a6dad34c5e4f2646d87905f005f8713489bf0f4d
Martin Zurowietz <martin@zurowietz.de>
2018-08-11 09:24:18 +0200

    Use mzur/kirby-defuse-session

--- tests/FormTest.php
+++ tests/FormTest.php
@@ -2,8 +2,8 @@
 
 namespace Jevets\Kirby\Form\Tests;
 
-use Kirby\Cms\App;
 use Jevets\Kirby\Form;
+use Mzur\Kirby\DefuseSession\Defuse;
 use Jevets\Kirby\Exceptions\TokenMismatchException;
 
 class FormTest extends TestCase
@@ -151,7 +151,7 @@ class FormTest extends TestCase
     public function testValidateCsrfException()
     {
         unset($_POST['csrf_token']);
-        App::instance(new SessionTestApp(['options' => ['debug' => true]]));
+        Defuse::defuse(['options' => ['debug' => true]]);
         csrf();
         $form = new Form;
         $this->expectException(TokenMismatchException::class);
@@ -161,7 +161,7 @@ class FormTest extends TestCase
     public function testValidateCsrfExceptionNoDebug()
     {
         unset($_POST['csrf_token']);
-        App::instance(new SessionTestApp(['options' => ['debug' => false]]));
+        Defuse::defuse(['options' => ['debug' => false]]);
         csrf();
         $form = new Form;
         $this->assertFalse($form->validates());
